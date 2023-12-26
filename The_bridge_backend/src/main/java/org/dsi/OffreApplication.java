f6d7859ec4fd30efdf3346223a3d06d044ad472e
TalelMejri <talelmejri8@gmail.com>
2023-12-26 22:50:17 +0100

    CorsConfigurationSource

--- The_bridge_backend/src/main/java/org/dsi/OffreApplication.java
+++ The_bridge_backend/src/main/java/org/dsi/OffreApplication.java
@@ -1,13 +1,30 @@
 package org.dsi;
 
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @SpringBootApplication
 public class OffreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OffreApplication.class, args);
+	}
+	
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+	    CorsConfiguration configuration = new CorsConfiguration();
+	    configuration.setAllowedOrigins(Arrays.asList("*"));
+	    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+	    configuration.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization"));
+	    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	    source.registerCorsConfiguration("/**", configuration);
+	    return source;
 	}
 
 }
